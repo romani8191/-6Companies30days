d68ee35ac8db62372fab349255334001ad77b5b7
Romani <95995818+romani8191@users.noreply.github.com>
2023-08-26 22:28:56 +0530

    Add files via upload

---
+++ Day7/pract 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void printArray(){
+
+}
+
+int main(){
+    int number[15];
+    cout<<"well done"<<endl;
+    cout<<"value at index 8 : "<<number[8]<<endl;
+
+    int second[3]={5,4,3};
+    cout<<"value at index 2 : "<<second[2]<<endl;
+
+
+}
