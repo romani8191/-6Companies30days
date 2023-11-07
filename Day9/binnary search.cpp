3978e8be7e3f689961ca09131c5bbc597a1f82ec
Romani <95995818+romani8191@users.noreply.github.com>
2023-11-08 00:32:59 +0530

    Add files via upload

---
+++ Day9/binnary search.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch( int arr[], int size, int key){
+    int start=0;
+    int end=size-1;
+
+    int mid=(start+end)/2;
+
+    while(start<=end){
+
+        if(arr[mid]==key){
+            return mid;
+        }
+
+        if(arr[mid]>key){
+            end=mid-1;
+        }
+
+        else{
+            start=mid+1;
+        }
+
+        mid=(start+end)/2;
+
+    }
+
+    return -1;
+}
+
+int main(){
+    int even[6]={2,4,6,8,10,12};
+    int odd[5]={1,3,5,7,9};
+
+    int index=BinarySearch(even,6,12);
+
+    cout << "Index of 12 is "<<index<<endl;
+
+    return 0;
+
+}
